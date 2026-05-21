9e4fbf98c457caea8b2f030e780dc98823e9cb96
Dym03 <hdymacek@gmail.com>
2026-05-21 23:38:00 +0200

    Hot reload for Svelte

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.middleware.csrf import get_token
 from django.shortcuts import render
 
 
 def index(request):
 	get_token(request)
-	return render(request, "core/index.html")
+	return render(request, "core/index.html", {"debug": settings.DEBUG}) # Pass debug flag to template for conditional frontend behavior
